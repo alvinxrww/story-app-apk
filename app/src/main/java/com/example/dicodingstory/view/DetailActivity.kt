5929471a208b6354fb9ac1e67c4a98f44ab07e1a
Alvin Xavier Rakha Wardhana <alvin.xavier@ui.ac.id>
2024-05-17 21:18:27 +0700

    fix: logout on details page

--- app/src/main/java/com/example/dicodingstory/view/DetailActivity.kt
+++ app/src/main/java/com/example/dicodingstory/view/DetailActivity.kt
@@ -1,22 +1,53 @@
 package com.example.dicodingstory.view
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
+import com.example.dicodingstory.R
 import com.example.dicodingstory.databinding.ActivityDetailBinding
+import com.example.dicodingstory.viewmodel.MainViewModel
+import com.example.dicodingstory.viewmodel.ViewModelFactory
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private val mainViewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout_button -> {
+                mainViewModel.logout()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mainViewModel.getSession().observe(this) { user ->
+            if (!user.isLogin) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }
+        }
 
         postponeEnterTransition()
 
